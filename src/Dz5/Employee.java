f4e09660aad7fd5269a640064f9db36c551cd565
Sergii-Korzhenko <ventoos1@gmail.com>
2021-07-14 22:33:55 +0300

    Add fields tarif, hour, name

---
+++ src/Dz5/Employee.java
@@ -0,0 +1,9 @@
+package Dz5;
+
+public class Employee {
+    public static void main(String[] args) {
+        double tarif =  5.85;
+        String name = "Ivan";
+        int hour = 135;
+    }
+}
