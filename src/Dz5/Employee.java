6339b799004eae6c281f78f90040bd4cdc7e055d
Sergii-Korzhenko <ventoos1@gmail.com>
2021-07-17 17:15:58 +0300

    Add method getBonus

--- src/Dz5/Employee.java
+++ src/Dz5/Employee.java
@@ -2,13 +2,21 @@ package Dz5;
 
 public class Employee {
     public static void main(String[] args) {
-        double tarif = 5.85;
+        double tarif = 15.85;
         String name = "Ivan";
         int hour = 135;
-
     }
 
     public static double getSalary(double tarif, int hour) {
         return tarif * hour;
+    }
+
+    public static double getBonus(double tarif, int hour) {
+        double salary = tarif * hour;
+        if (salary > 2000) {
+            return salary * 1.2;
+        } else {
+            return salary;
+        }
     }
 }
